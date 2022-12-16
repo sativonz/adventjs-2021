a7d38940172b98d7ca4bcb012fba0c69daca2421
sativonz <fernandogarciafornes@gmail.com>
2022-12-16 08:44:48 +0100

    Reto 2

---
+++ Retos/Reto-2/index.js
@@ -0,0 +1,12 @@
+//bici coche balón _playstation bici coche peluche muñeco
+
+export default function listGifts(carta) {
+    let regalos = carta.match(/\b[0-9-_a-zA-ZÀ-ÿu00f1u00d1a-zA-ZÀ-ÿu00f1u00d1]+\b/g);
+    regalos = regalos.filter(regalo => !regalo.startsWith("_"));
+    const counts = regalos.reduce((acc, value) => ({
+        ...acc,
+        [value]: (acc[value] || 0) + 1
+     }), {});
+     return counts;
+
+}
--- Retos/main.js
+++ Retos/main.js
@@ -1,4 +1,5 @@
-import contarOvejas from "./Reto-1/index.js";
+//import contarOvejas from "./Reto-1/index.js";
+import listGifts from "./Reto-2/index.js";
 
 // Reto 1
 const ovejas = [
@@ -9,4 +10,9 @@ const ovejas = [
     { name: 'AAAAAaaaaa', color: 'rojo' },
     { name: 'Nnnnnnnn', color: 'rojo'}
   ]
-console.log(contarOvejas(ovejas))
+//console.log(contarOvejas(ovejas))
+
+
+// Reto 2
+const carta = 'bici coche balón _playstation   bici coche peluche muñeco'
+console.log(listGifts(carta));
